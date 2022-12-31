e4a82307ee7ea692423c468c518e2d405b845312
Matt Goldman <matt.goldman@outlook.com>
2022-12-31 20:39:41 +1100

    Initial commit

---
+++ UI/Services/WeatherService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using DependencyTest.Shared;
+
+namespace DependencyTest.UI.Services;
+
+public class WeatherService
+{
+	private readonly HttpClient httpClient;
+
+	public WeatherService(IHttpClientFactory factory)
+	{
+		httpClient = factory.CreateClient();
+	}
+
+	public async Task<IEnumerable<WeatherForecast>> GetWeather()
+	{
+		return await httpClient.GetFromJsonAsync<List<WeatherForecast>>("WeatherForecast");
+	}
+}
+
